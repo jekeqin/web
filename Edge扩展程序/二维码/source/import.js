cd4c02c45bf4981fd2f7e4002c45e01232bdeeb8
jekeqin <296414452@qq.com>
2022-07-07 14:13:27 +0800

    update

--- Edge扩展程序/二维码/source/import.js
+++ Edge扩展程序/二维码/source/import.js
@@ -2,12 +2,15 @@
 // 注入网页的js，响应扩展发送过来的事件
 
 chrome.runtime.onMessage.addListener(function(request, sender, callback) {
-    console.log(request);
+    // 注入到网页的JS，接收到请求之后，执行相应操作
+    //console.log(request);
     if( request.event=='url' ){
         callback(location.href);
-    }else{
-        callback("This message is from content.js");
+    }
+    else
+    {
+        callback("This message is from import.js");
     }
 });
 
-console.log("import true");
+//console.log("import.js true");
--- Edge扩展程序/二维码/source/site.js
+++ Edge扩展程序/二维码/source/site.js
@@ -1,4 +1,6 @@
 
+// https://www.cnblogs.com/liuxianan/p/chrome-plugin-develop.html
+// https://developer.mozilla.org/zh-CN/docs/Mozilla/Add-ons/WebExtensions/API
 
 // 关闭扩展
 // window.close();
@@ -7,29 +9,54 @@
 // qrcode.makeCode("http://www.w3cschool.cc"); // 生成另外一个二维码
 
 var coder = new QRCode("code", { width: 180, height: 180, correctLevel : QRCode.CorrectLevel.L });
-var textarea = document.getElementById("url");
+
 
 function createCode(text){
     try{
         coder.clear();
-        coder.makeCode(text);
+        if( (text && text.length>0) || text===0  )
+            coder.makeCode(text);
     }catch(e){
 
     }
 }
 
-
-
+var textarea = document.getElementById("url");
 if(textarea){
     textarea.onblur = function(){
         createCode( textarea.value );
     }
+    textarea.onkeydown = function(e){
+        if(e && e.keyCode==13){
+            createCode( textarea.value );
+        }
+    }
 }
 
+
 chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    // 扩展加载完成后，向网页发送请求
     chrome.tabs.sendMessage(tabs[0].id, {event:'url'}, function(url){
+        if( url==undefined ){
+            url = '';
+        }
         createCode(url);
         textarea.value = url;
     });
 });
 
+var lamp = document.getElementById("lamp");
+if( lamp ){
+    console.log("qrcode.import.js",chrome.i18n.getMessage("button"));
+
+    chrome.storage.sync.get({closeLamp:false}, function(obj){
+        console.log(obj);
+        lamp.checked = obj.closeLamp;
+    });
+    lamp.addEventListener('change', function(){
+        chrome.storage.sync.set({closeLamp:lamp.checked});
+        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {event:'lamp', tabId:tabs[0].id});
+        });
+    });
+}
